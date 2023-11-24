075d6c90924b0f7e26d76c6910e910a781d7ce4d
suian <suian@DESKTOP-8K1OL8K>
2023-11-24 12:06:53 -0300

    Adicionar arquivos de projeto.

---
+++ marketplace02/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(marketplace02.Startup))]
+namespace marketplace02
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
